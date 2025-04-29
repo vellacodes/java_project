0426fb595da14838e1566f23d819e29ba8f5a215
vellacodes <vellabatdorf@gmail.com>
2025-04-29 09:47:48 -0500

    new

---
+++ .vscode/Employee.java
@@ -0,0 +1,15 @@
+
+
+
+public class Employee {
+    private String username;
+    private String password;
+    private String role;
+
+    public Employee(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+    
+}
--- .vscode/LoginPage.java
+++ .vscode/LoginPage.java
@@ -1,13 +1,12 @@
 //for the login page graphics, we used example code from stackoverflow.com
-//
 
-
+//imports for all the graphics, buttons, and readers we may need
 import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.nio.Buffer;
 
 import javax.swing.ImageIcon;
@@ -33,7 +32,7 @@ public class LoginPage extends JFrame implements ActionListener{
     private JButton resetButton;
     private JCheckBox showPassword;
 
-
+//creates graphics for new login page
     public LoginPage() {
 //        container = getContentPane();
         userLabel = new JLabel("USERNAME");
@@ -109,8 +108,9 @@ public class LoginPage extends JFrame implements ActionListener{
         showPassword.addActionListener(this);
     }
 
-    public void /*loginA*/actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {
         //Coding Part of LOGIN button
+        //getting text entered by the user
         if (e.getSource() == loginButton) {
             String userText;
             String pwdText;
@@ -119,42 +119,70 @@ public class LoginPage extends JFrame implements ActionListener{
             //change to cycle through the CSV file to find username and password.
             
             //cycle through CSV file usernames.
-            /*if(username is found) something like employee[i](userInfo[0]) = input from username)
-            then check if CORRESPONDING password is correct.- employee[i](userInfo[1]) = input from password
-            then get role from employee[i](userInfo[2]) and take to page based on role.
-            //if not found, then show message that username and password are incorrect.
-            */
             String file = "vscode\\users.csv";//file path to csv file
-            BufferedReader reader = null//BufferedReader to read the file
-            String line = newFile.NewLine();
+            BufferedReader reader = null;//BufferedReader to read the file
+            String line = "";
             try{
-                reader = new BufferedReader(new FileReader(fileName));
-                while((line = reader.readLine()) ! = null){
-                    String[] row = line.split(",");//splits strings into array of strings based on commas
-                    for(String index : row){
+                reader = new BufferedReader(new FileReader(file));
+                while((line = reader.readLine()) != null){
+                    //need to set employee info to the entire line, THEN split it by commas to get user, pass, and role
+                    
+                    String[] employee[i] = line.split(",");//splits strings into array of strings based on commas
+                    for(String index : employee){
                         System.out.printf("%10s", index); //print out the index of the row to see if it works
                     }
                 }
-            } catch(Exception e){
-                e.printStackTrace();//displays what exactly went wrong
+            } catch(Exception ex){
+                System.out.println(ex);//displays what exactly went wrong
             }
             finally{
                 try{
                 reader.close();//close reader.
-                } catch(Exception e){
-                    e.printStackTrace();//displays what went wrong
+                } catch(Exception exe){
+                    System.out.println(exe);//displays what went wrong
                 }
             }
-                String[]userInfo = line.Split(“,”); //separates wherever there is a comma, splits data into array of strings to access easier
-                ArrayList(employees) users //from employee class? List of role, user, and pass
+                Employee[] employees = line.split(","); //separates wherever there is a comma, splits data into array of strings to access easier
+                ArrayList<String> employees = new ArrayList <String>(); //from employee class? List of role, user, and pass
                 Employee e = newEmployee(userInfo(0), userInfo(1), userInfo(2));
                 users.append(e); //add new employee to array list, can use for new users or remove to fire people.
                 //After ending program (closing) write n
 
+            /*if(username is found) something like employee[i](userInfo[0]) = input from username)
+            then check if CORRESPONDING password is correct.- employee[i](userInfo[1]) = input from password
+            then get role from employee[i](userInfo[2]) and take to page based on role.
+            //if not found, then show message that username or password is incorrect.
+            */
+
+            //for loop 
+            //if employyee[i]userInfo[0] = username input(
+            //if employee[i]userInfo[1] = password input)
+            //take to page
 
 
+            /*
+            array list of employee objects
+            
+            
+            loop through csv file
+            //read line into some variable
+            string array split on commas\
+            creates a size array of strings 
+            read line one before loop b/.c not a person
+
+            */
+            for(int i = 0; i < employees.size(); i++){
+                if(userText.equalsIgnoreCase(employee[i].userInfo[0])){
+                    if(pwdText.equalsIgnoreCase(employee[i].userInfo[1])){
+                        //log in and find role
+                    }else{
+                        JOptionPane.showMessageDialog("Invalid Password");
+                    }
+                JOptionPane.showMessageDialog("Invalid Username");
+                }
             }
 
+            }
 
 
             if (userText.equalsIgnoreCase("Admin") && pwdText.equalsIgnoreCase("Admin")) {
@@ -177,10 +205,5 @@ public class LoginPage extends JFrame implements ActionListener{
                 passwordField.setEchoChar('*');
             }
         }        
-    }
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            new TestFrame().login();
-        });
     }
 }
